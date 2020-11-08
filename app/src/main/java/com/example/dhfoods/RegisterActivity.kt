04c22698c23d28262905cf4dc96fef1e0cc04e63
Mateus <mateustrindade8@gmail.com>
2020-11-08 12:28:04 -0300

    Inclusão de scrollview nos layouts activity_login.xml e activity_register.xml e implementação do navigation da RegisterActivity.kt

--- app/src/main/java/com/example/dhfoods/RegisterActivity.kt
+++ app/src/main/java/com/example/dhfoods/RegisterActivity.kt
@@ -2,13 +2,15 @@ package com.example.dhfoods
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        setSupportActionBar(findViewById(R.id.tb_register))
+        setSupportActionBar(tb_register)
+        tb_register.setNavigationOnClickListener { finish() }
 
     }
 }
