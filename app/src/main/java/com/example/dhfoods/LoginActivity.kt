24f9e690c7300bdab6820105b130d83cf2db6da7
Mateus <mateustrindade8@gmail.com>
2020-11-08 00:33:58 -0300

    Inclusão da activity_register.xml.

--- app/src/main/java/com/example/dhfoods/LoginActivity.kt
+++ app/src/main/java/com/example/dhfoods/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.dhfoods
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnRegister.setOnClickListener{
+            callRegisterActiviy()
+        }
+
     }
+
+    fun callRegisterActiviy(){
+        var intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
 }
---
+++ app/src/main/java/com/example/dhfoods/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dhfoods
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        setSupportActionBar(findViewById(R.id.tb_register))
+
+    }
+}
